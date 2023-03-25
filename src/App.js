c6d304b9a39f19074db2932582ebbb1258048e96
Jayanth <jayanth_bonthala@srmap.edu.in>
2023-03-25 18:54:27 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(false);
+  const abc = "This is Jayanth";
+  const d = "This is sowmya";
+  const click = () => {
+    setValue(!value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {<h1 className={value ? "app-h1" : "app-h"}>{value ? abc : d}</h1>}
+      <button id="button" onClick={click}>
+        click
+      </button>
+      <table  className="tabl">
+        <thead>
+          <tr>
+            <th>col 1</th>
+            <th>col 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <tr>
+                <td>
+                  1
+                </td>
+                <td>
+                  1.5
+                </td>
+              </tr>
+            </td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+      <h1 style={{color:"red"}}>
+      color
+      </h1>
     </div>
   );
 }
